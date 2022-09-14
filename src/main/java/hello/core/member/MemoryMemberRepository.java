517b72946cc2f5efb9f66cc7012c0f411e7c3495
minseojo <64322765+minseojo@users.noreply.github.com>
2022-09-15 02:10:06 +0900

    Understanding Spring Core Principles 1 - Creating Examples

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); //실제로는 해쉬맵안씀 동시성 문제떄문에 ConcurrentHashMap쓴다
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
